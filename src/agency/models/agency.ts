7eed7408909064c3c0a88723db3f2ff9a04cb30f
Chan <chan.web.work@gmail.com>
2025-08-03 19:41:11 +0800

    Delete src directory

--- src/agency/models/agency.ts
+++
@@ -1,8 +0,0 @@
-export interface Agency {
-  id: number;
-  franchisor_id?: number;
-  name: string;
-  address?: string;
-  created_at?: Date | string;
-  updated_at?: Date | string;
-}
--- src/franchisor/models/franchisor.ts
+++
@@ -1,6 +0,0 @@
-export interface Franchisor {
-  id: number;
-  name: string;
-  created_at?: Date | string;
-  updated_at?: Date | string;
-}
